79be0bea729acbfe98ff0cf885a0b100d54498b1
Carlos Garcia <chenzhen.dev@gmail.com>
2024-11-26 22:44:30 -0300

    feat(database): instalção de ORM(Mongoose) e criando conexão

--- code/back-end/src/server.js
+++ code/back-end/src/server.js
@@ -1,6 +1,16 @@
+const dotenv = require('dotenv')
+dotenv.config({ path: '.env' })
+const mongoose = require('mongoose')
 const app  = require('./app')
 
-//Server
+//-Database
+const DB_CONN = process.env.DB_LOCAL_CONN
+mongoose
+.connect(DB_CONN)
+.then(conn => console.log('Conexão com banco realizado com sucesso!'))
+.catch(err => console.log('Erro ao conectar com Banco de Dados'))
+
+//-Server
 const port = 8000
 app.listen(port, () => 
     console.log(`Server run on port ${port}`)
