41b1d3b3094ba143ac0df4c6c9240380bbd25517
Carlos Garcia <chenzhen.dev@gmail.com>
2024-11-27 03:15:41 -0300

    feat(crud-despesa): cadastro de despesa

--- code/back-end/src/controllers/expenseControllers.js
+++ code/back-end/src/controllers/expenseControllers.js
@@ -1,25 +1,113 @@
-function getExpense(req, res) {
-    console.log('Listar de despesas...') 
+const Expense = require('../models/expenseModels')
+
+async function getExpense(req, res) { 
+    const queryParams = req.query
+    try {
+        const expense = await Expense.find(queryParams)
+        res
+        .status(200)
+        .json({
+            status: 'success',
+            message: 'Despesa(s) obtida com sucesso!',
+            data: expense
+        })
+    } catch (error) {
+        res
+        .status(400)
+        .json({
+            status: 'fail',
+            message: 'Falha ao obter despesa(s)!',
+            data: error.errors
+        })
+    } 
 }
 
-function getExpenseById(req, res) {
-    console.log(`Lista despesas para ${req.params.id}`) 
+async function getExpenseById(req, res) {
+    try {
+        const expense = await Expense.findById(req.params.id)
+        res
+        .status(200)
+        .json({
+            status: 'success',
+            message: 'Despesa obtida com sucesso!',
+            data: expense
+        })
+    } catch (error) {
+        res
+        .status(400)
+        .json({
+            status: 'fail',
+            message: `Falha ao obter Despesa para id: ${req.params.id}`,
+        })
+    }  
 }
 
-function createExpense(req, res) {
-    console.log('Cadastrar despesas...')
-    const expenseData = req.body
-    console.log(expenseData) 
+async function createExpense(req, res) {
+    const data = req.body
+    try {
+        const expense = await Expense.create(data)
+        res
+        .status(201)
+        .json({
+            status: 'success',
+            message: 'Despesa criada com sucesso!',
+            data: expense
+        })
+    } catch (error) {
+        console.log(error)
+        res
+        .status(400)
+        .json({
+            status: 'fail',
+            message: error.errors.type.message,
+            data: error.errors.type
+        })
+    } 
 }
 
-function updateExpense(req, res) {
-    console.log(`Atualizar despesa -> ${req.params.id}`)
-    const expenseData = req.body
-    console.log(expenseData) 
+async function updateExpense(req, res) {
+    const id = req.params.id
+    const data = req.body
+    const opt =  {
+        new: true,
+        runValidators: true,
+    }
+    try {
+        const expense = await Expense.findByIdAndUpdate(id, data, opt)
+        res
+        .status(200)
+        .json({
+            status: 'success',
+            message: 'Despesa atualizada com sucesso!',
+            data: expense
+        })
+    } catch (error) {
+        res
+        .status(400)
+        .json({
+            status: 'fail',
+            message: `Falha ao atualizar despesa: ${id}`,
+        })
+    } 
 }
 
-function deleteExpense(req, res) {
-    console.log(`Deletar despesa -> ${req.params.id}`)
+async function deleteExpense(req, res) {
+    try {
+        const expense = await Expense.findByIdAndDelete(req.params.id)
+        res
+        .status(200)
+        .json({
+            status: 'success',
+            message: 'Despesa deletada com sucesso!',
+        })
+    } catch (error) {
+        res
+        .status(400)
+        .json({
+            status: 'fail',
+            message: `Falha ao deletar despesa!`,
+        })
+    }
 }
 
 module.exports = {
